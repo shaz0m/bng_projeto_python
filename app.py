f691082e5ac3ea7e3b8bba988917e8d937ca07cd
Lee Martins <lara.for.work.email@gmail.com>
2026-06-01 11:55:55 +0100

    Implement user profile route with database access
    Add profile route to fetch user data from database

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import mysql.connector
+from bd import obter_ligacao
 
 app = Flask(__name__)
 
@@ -9,6 +11,22 @@ def inicio():
 @app.route('/login')
 def login(): 
     return render_template('login.html')
+
+@app.route('/perfil/<id>', methods = ["GET"])
+def perfil(id):
+    bd = obter_ligacao()
+    cursor = bd.cursor(dictionary=True)
+
+    cursor.execute("select * from utilizador where id = %s", (id))
+    utili = cursor.fetchone()
+
+    cursor.execute("select count(*) from elementocolecao where idUtilizador = %s", (id))
+    numJogos = cursor.fetchone()
+    
+    cursor.close()
+    bd.close()
+    return render_template('perfil.html', utili=utili, numJogos = numJogos)
+
 
 if __name__ == "__main__" :
     app.run(debug=True)
