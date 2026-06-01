d27d462f8b67f6de818d56c9351cb12a686c627f
Lee Martins <lara.for.work.email@gmail.com>
2026-06-01 11:54:32 +0100

    Add number of games to user profile rendering

--- app.py
+++ app.py
@@ -20,9 +20,13 @@ def perfil(id):
     cursor.execute("select * from utilizador where id = %s", (id))
     utili = cursor.fetchone()
 
+    cursor.execute("select count(*) from elementocolecao where idUtilizador = %s", (id))
+    numJogos = cursor.fetchone()
+    
     cursor.close()
     bd.close()
-    return render_template('perfil.html', utili=utili)
+    return render_template('perfil.html', utili=utili, numJogos = numJogos)
+
 
 if __name__ == "__main__" :
     app.run(debug=True)
