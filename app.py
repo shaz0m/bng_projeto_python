46c4027c0dee86947d20eeb70f6542fdeee3bcd1
Ana Ferraz de Freitas <anafrei2024@gmail.com>
2026-06-04 18:05:58 +0100

    Add routes for todos os jogos and add/remove fave, posse, etc

--- app.py
+++ app.py
@@ -125,6 +125,205 @@ def perfil():
     bd.close()
     return render_template('perfil.html', utili=utili, numJogos=numJogos)
 
+# -- Todos Jogos -------------------------------------------
+@app.route('/todos-jogos', methods=["GET"])
+def todos_jogos():
+     # Proteção
+    if "id" not in session:
+        return redirect("/login")
+    
+    bd = obter_ligacao()
+    cursor = bd.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM utilizador WHERE id = %s", (session["id"],))
+    utili = cursor.fetchone()
+
+    cursor.execute("select * from jogo LIMIT 10")
+    jogos = cursor.fetchall()
+
+    cursor.execute("SELECT idJogo FROM elementocolecao WHERE idUtilizador = %s AND favorito = 1", (session["id"],))
+    favoritos_db = cursor.fetchall()
+    favoritos = {row["idJogo"] for row in favoritos_db}
+
+    cursor.execute("SELECT idJogo FROM elementocolecao WHERE idUtilizador = %s AND jogado = 1", (session["id"],))
+    jogados_db = cursor.fetchall()
+    jogados = {row["idJogo"] for row in jogados_db}
+
+    cursor.execute("SELECT idJogo FROM elementocolecao WHERE idUtilizador = %s AND posse = 1", (session["id"],))
+    possui_db = cursor.fetchall()
+    possui = {row["idJogo"] for row in possui_db}
+
+    cursor.execute("SELECT idJogo FROM elementocolecao WHERE idUtilizador = %s", (session["id"],))
+    colecao_db = cursor.fetchall()
+    colecao = {row["idJogo"] for row in colecao_db}
+
+    cursor.close()
+    bd.close()
+
+    return render_template("todosJogos.html", utili=utili, jogos = jogos, favoritos = favoritos, jogados = jogados, possui = possui, colecao = colecao)
+
+# -- Remover da Coleção -------------------------------------------
+@app.route('/remover-colecao', methods=["POST"])
+def remover_colecao():
+    if "id" not in session:
+        return redirect("/login")
+
+    idJogo = request.form.get('jogo_id')
+
+    bd = obter_ligacao()
+    cursor = bd.cursor()
+
+    cursor.execute("DELETE FROM elementocolecao WHERE idUtilizador = %s AND idJogo = %s", (session["id"], idJogo))
+    bd.commit()
+
+    cursor.close()
+    bd.close()
+
+    flash("Jogo removido da coleção.")
+    return redirect("/todos-jogos")
+
+# -- Adicionar à Coleção -------------------------------------------
+@app.route('/adicionar-colecao', methods=["POST"])
+def adicionar_colecao():
+    if "id" not in session:
+        return redirect("/login")
+
+    idJogo = request.form.get('jogo_id')
+
+    bd = obter_ligacao()
+    cursor = bd.cursor()
+
+    cursor.execute("INSERT INTO elementocolecao (idUtilizador, idJogo, posse, jogado, favorito) VALUES (%s, %s, %s, %s, %s)", (session["id"], idJogo, 0, 0, 0))
+    bd.commit()
+
+    cursor.close()
+    bd.close()
+
+    flash("Jogo adicionado à coleção.")
+    return redirect("/todos-jogos")
+
+# -- Adicionar à Favoritos -------------------------------------------
+@app.route('/adicionar-favoritos', methods=["POST"])
+def adicionar_favoritos():
+    if "id" not in session:
+        return redirect("/login")
+
+    idJogo = request.form.get('jogo_id')
+
+    bd = obter_ligacao()
+    cursor = bd.cursor()
+
+    cursor.execute("UPDATE elementocolecao SET favorito = 1 WHERE idUtilizador = %s AND idJogo = %s", (session["id"], idJogo))
+    bd.commit()
+
+    cursor.close()
+    bd.close()
+
+    flash("Jogo adicionado aos favoritos.")
+    return redirect("/todos-jogos")
+
+# -- Remover dos Favoritos -------------------------------------------
+@app.route('/remover-favoritos', methods=["POST"])
+def remover_favoritos():
+    if "id" not in session:
+        return redirect("/login")
+
+    idJogo = request.form.get('jogo_id')
+
+    bd = obter_ligacao()
+    cursor = bd.cursor()
+
+    cursor.execute("UPDATE elementocolecao SET favorito = 0 WHERE idUtilizador = %s AND idJogo = %s", (session["id"], idJogo))
+    bd.commit()
+
+    cursor.close()
+    bd.close()
+
+    flash("Jogo removido dos favoritos.")
+    return redirect("/todos-jogos")
+
+
+# -- Adicionar à Posse -------------------------------------------
+@app.route('/adicionar-posse', methods=["POST"])
+def adicionar_posse():
+    if "id" not in session:
+        return redirect("/login")
+
+    idJogo = request.form.get('jogo_id')
+
+    bd = obter_ligacao()
+    cursor = bd.cursor()
+
+    cursor.execute("UPDATE elementocolecao SET posse = 1 WHERE idUtilizador = %s AND idJogo = %s", (session["id"], idJogo))
+    bd.commit()
+
+    cursor.close()
+    bd.close()
+
+    flash("Jogo adicionado à posse.")
+    return redirect("/todos-jogos")
+
+# -- Remover da Posse -------------------------------------------
+@app.route('/remover-posse', methods=["POST"])
+def remover_posse():
+    if "id" not in session:
+        return redirect("/login")
+
+    idJogo = request.form.get('jogo_id')
+
+    bd = obter_ligacao()
+    cursor = bd.cursor()
+
+    cursor.execute("UPDATE elementocolecao SET posse = 0 WHERE idUtilizador = %s AND idJogo = %s", (session["id"], idJogo))
+    bd.commit()
+
+    cursor.close()
+    bd.close()
+
+    flash("Jogo removido da posse.")
+    return redirect("/todos-jogos")
+
+# -- Adicionar à Jogados -------------------------------------------
+@app.route('/adicionar-jogados', methods=["POST"])
+def adicionar_jogados():
+    if "id" not in session:
+        return redirect("/login")
+
+    idJogo = request.form.get('jogo_id')
+
+    bd = obter_ligacao()
+    cursor = bd.cursor()
+
+    cursor.execute("UPDATE elementocolecao SET jogado = 1 WHERE idUtilizador = %s AND idJogo = %s", (session["id"], idJogo))
+    bd.commit()
+
+    cursor.close()
+    bd.close()
+
+    flash("Jogo adicionado aos jogados.")
+    return redirect("/todos-jogos")
+
+# -- Remover dos Jogados -------------------------------------------
+@app.route('/remover-jogados', methods=["POST"])
+def remover_jogados():
+    if "id" not in session:
+        return redirect("/login")
+
+    idJogo = request.form.get('jogo_id')
+
+    bd = obter_ligacao()
+    cursor = bd.cursor()
+
+    cursor.execute("UPDATE elementocolecao SET jogado = 0 WHERE idUtilizador = %s AND idJogo = %s", (session["id"], idJogo))
+    bd.commit()
+
+    cursor.close()
+    bd.close()
+
+    flash("Jogo removido dos jogados.")
+    return redirect("/todos-jogos")
+
+
 if __name__ == "__main__":
     app.secret_key = "01031315"
     app.config['SESSION_TYPE'] = 'filesystem'
